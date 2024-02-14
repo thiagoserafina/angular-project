3752e40e9385de8bf2ee7c6582852feadb57750c
thiagoserafina <thiago.serafina@outlook.com>
2024-02-14 17:02:25 -0300

    feat/login: implementação do serviço de autenticação

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,27 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { TopbarModule } from '../../components/topbar/topbar.module';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [TopbarModule],
+  imports: [TopbarModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
-  loginForm = new FormGroup({
-    email: new FormControl(''),
+export class LoginComponent implements OnInit {
+  authService: AuthService = inject(AuthService);
+  router: Router = inject(Router);
+  activeRoute: ActivatedRoute = inject(ActivatedRoute);
+
+  login = new FormGroup({
+    username: new FormControl(''),
     password: new FormControl(''),
   });
 
-  formGroup = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
-  });
+  ngOnInit(): void {
+    this.activeRoute.queryParamMap.subscribe((queries) => {
+      const logout = Boolean(queries.get('logout'));
 
-  onSubmit(e: Event) {
-    e.preventDefault();
-    console.log(this.loginForm.value);
+      if (logout) {
+        this.authService.logout();
+      }
+    });
+  }
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+
+    const user = this.authService.login(
+      this.login.value.username,
+      this.login.value.password
+    );
+
+    if (user === undefined) {
+      console.log('Login failed');
+    } else {
+      console.log('Login successful');
+      this.router.navigate(['/home']);
+    }
   }
 }
